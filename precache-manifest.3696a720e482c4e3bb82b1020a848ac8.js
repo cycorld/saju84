9cd68427dcb489a3938786f77312915b778ec6ae
cycorld <charleschoi87@gmail.com>
2020-03-21 23:26:42 +0900

    init

---
+++ precache-manifest.3696a720e482c4e3bb82b1020a848ac8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d33c8079fe4bf3b0994ba4bd8cfb801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69201209b2582a88d6ce",
+    "url": "/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "eefb50e8e1666e1df9c8",
+    "url": "/static/css/main.408afe60.chunk.css"
+  },
+  {
+    "revision": "69201209b2582a88d6ce",
+    "url": "/static/js/2.0cb1ff7f.chunk.js"
+  },
+  {
+    "revision": "049a705bc03967bbc06bc6c79b12d77d",
+    "url": "/static/js/2.0cb1ff7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eefb50e8e1666e1df9c8",
+    "url": "/static/js/main.5ed2fbe1.chunk.js"
+  },
+  {
+    "revision": "5365e5b553cf414c53ac",
+    "url": "/static/js/runtime-main.46abfee8.js"
+  },
+  {
+    "revision": "d4e512f90f9401e0094ab6f4faf84f9e",
+    "url": "/static/media/no.d4e512f9.svg"
+  },
+  {
+    "revision": "57cc48d7728fda0a511865c2d5a5adec",
+    "url": "/static/media/yes.57cc48d7.svg"
+  }
+]);
